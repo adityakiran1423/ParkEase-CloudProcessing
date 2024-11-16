df4b95cf65a307749c45b0f0066588bb7d123e9a
adityakiran1423 <adi.kiran@gmail.com>
2024-11-16 19:29:38 +0530

    imports added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # fetches live stream from ip camera
 
+import cv2
+import numpy as np
+import pickle
+import pandas as pd
+from ultralytics import YOLO
+import cvzone
+
 def main():
-    
+    # video capture loop
     pass
 
 if __name__ == "__main__":
